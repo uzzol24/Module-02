b15b99671c113d47b1c5077e8e92a1fb8e5a349a
uzzol24 <uzzalbanik.freelancer@gmail.com>
2023-12-16 17:01:19 +0600

    Done

---
+++ Tasktwo.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task Two</title>
+</head>
+<body>
+    
+
+<?php
+ $Name="Uzzol Datta";
+ $email="ujjalbanik543@gmail.com";
+ $Namver_one=03030303030;
+ $rol=2020.11111;
+ $SECTION="";
+ $float=0;
+
+ echo var_dump($Name);
+ echo var_dump($rol);
+ echo var_dump($Namver_one);
+ echo var_dump($email);
+ echo var_dump($float);
+ echo var_dump($SECTION);
+
+?>
+
+<?php
+$name="Ujjal Banik";
+$e_mail="ujjalbanik543@gmail.com";
+$phone= 01935098073; 
+ 
+echo strrev($name);
+echo str_word_count($e_mail);
+echo strlen($phone);
+
+?>
+
+
+
+
+
+
+
+
+</body>
+</html>
