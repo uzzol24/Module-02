ffeaf1455395916f0b13c94f41151f139a654096
uzzol24 <uzzalbanik.freelancer@gmail.com>
2023-12-16 16:55:34 +0600

    Task 1

---
+++ Taskone.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Task 01</title>
+</head>
+    <body>
+
+        <?PHP
+            $name="Uzzol Datta";
+            $email="ujjalbanik543@gmail.com";
+            $course_name="PHP & Laravel Programmers";
+            $Module_name="Mastering in PHP & MySQL";
+            
+            echo $name ."<br>" , $email ."<br>". $course_name ."<br>". $Module_name; 
+        ?>
+
+    </body>
+
+</html>
