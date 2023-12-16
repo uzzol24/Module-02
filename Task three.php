b23c7628b889d5266783b1e41bfb771562b87a48
uzzol24 <uzzalbanik.freelancer@gmail.com>
2023-12-16 17:09:12 +0600

    2 done

---
+++ Task three.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+ $name="Uzzol Datta";
+ $e_mail="ujjalbanik543@gmail.com";
+ 
+echo strrev($Name);
+echo str_word_count($e_mail);
+?>
+
+<?php
+
+$Name="Uzzol Datta";
+$email="ujjalbanik543@gmail.com";
+$Rol=520.50;
+
+ echo  var_dump($Rol);
+ echo strlen($university);
+ echo strrev($university);
+
+ echo str_word_count($university);
+ echo var_dump($Rol) ."<br>". strrev($university) ."<br>". str_word_count($university);
+
+?>
+
+</body>
+</html>
